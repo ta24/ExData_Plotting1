ff8bad864f681d2f2c5ad6ee5a078d7d1957a0de
Peter Lee <ta24@users.noreply.github.com>
2016-11-14 12:51:29 -0500

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,38 @@
+Plot1 <- function(){
+    data1 <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+    date1 <- data1$Date
+    count <- 0
+    
+    for (i in 1: length(date1)) {
+        if (date1[i] == "1/2/2007" | date1[i] == "2/2/2007") {
+            if (count != 0) {
+                count = count + 1
+                Date1[count] <- as.Date(data1$Date[i], "%d/%m/%Y")
+                Date2[count] <- strptime(paste(data1$Date[i], data1$Time[i]), "%d/%m/%Y %H:%M:%S")
+                Global_active_power[count] <- as.numeric(as.character(data1$Global_active_power[i]))
+                Global_reactive_power[count] <- as.numeric(as.character(data1$Global_reactive_power[i]))
+                Sub_Metering_1[count] <- as.numeric(as.character(data1$Sub_metering_1[i]))
+                Sub_Metering_2[count] <- as.numeric(as.character(data1$Sub_metering_2[i]))
+                Sub_Metering_3[count] <- as.numeric(as.character(data1$Sub_metering_3[i]))
+                Voltage[count] <- as.numeric(as.character(data1$Voltage[i]))
+            }
+            if (count == 0) {
+                count = count + 1
+                Date1 <- as.Date(data1$Date[i], "%d/%m/%Y")
+                Date2 <- strptime(paste(data1$Date[i], data1$Time[i]), "%d/%m/%Y %H:%M:%S")
+                Global_active_power <- as.numeric(as.character(data1$Global_active_power[i]))
+                Global_reactive_power <- as.numeric(as.character(data1$Global_reactive_power[i]))
+                Sub_Metering_1 <- as.numeric(as.character(data1$Sub_metering_1[i]))
+                Sub_Metering_2 <- as.numeric(as.character(data1$Sub_metering_2[i]))
+                Sub_Metering_3 <- as.numeric(as.character(data1$Sub_metering_3[i]))
+                Voltage <- as.numeric(as.character(data1$Voltage[i]))
+            }
+        }
+    }
+    
+    hist(Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main ="Global Active Power")
+    
+    png(file = "Plot1.png")
+    hist(Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main ="Global Active Power")
+    dev.off()
+}
---
+++ Plot3.R
@@ -0,0 +1,46 @@
+Plot3 <- function(){
+    data1 <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+    date1 <- data1$Date
+    count <- 0
+    
+    for (i in 1: length(date1)) {
+        if (date1[i] == "1/2/2007" | date1[i] == "2/2/2007") {
+            if (count != 0) {
+                count = count + 1
+                Date1[count] <- as.Date(data1$Date[i], "%d/%m/%Y")
+                Date2[count] <- strptime(paste(data1$Date[i], data1$Time[i]), "%d/%m/%Y %H:%M:%S")
+                Global_active_power[count] <- as.numeric(as.character(data1$Global_active_power[i]))
+                Global_reactive_power[count] <- as.numeric(as.character(data1$Global_reactive_power[i]))
+                Sub_Metering_1[count] <- as.numeric(as.character(data1$Sub_metering_1[i]))
+                Sub_Metering_2[count] <- as.numeric(as.character(data1$Sub_metering_2[i]))
+                Sub_Metering_3[count] <- as.numeric(as.character(data1$Sub_metering_3[i]))
+                Voltage[count] <- as.numeric(as.character(data1$Voltage[i]))
+            }
+            if (count == 0) {
+                count = count + 1
+                Date1 <- as.Date(data1$Date[i], "%d/%m/%Y")
+                Date2 <- strptime(paste(data1$Date[i], data1$Time[i]), "%d/%m/%Y %H:%M:%S")
+                Global_active_power <- as.numeric(as.character(data1$Global_active_power[i]))
+                Global_reactive_power <- as.numeric(as.character(data1$Global_reactive_power[i]))
+                Sub_Metering_1 <- as.numeric(as.character(data1$Sub_metering_1[i]))
+                Sub_Metering_2 <- as.numeric(as.character(data1$Sub_metering_2[i]))
+                Sub_Metering_3 <- as.numeric(as.character(data1$Sub_metering_3[i]))
+                Voltage <- as.numeric(as.character(data1$Voltage[i]))
+            }
+        }
+    }
+    
+    plot(Date2, Sub_Metering_1, type = "n", lty = 1, xlab = "", ylab = "Energy sub metering")
+    lines(Date2, Sub_Metering_1, col = "black")
+    lines(Date2, Sub_Metering_2, col = "red")
+    lines(Date2, Sub_Metering_3, col = "blue")
+    legend( "topright", c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3"), lty = c(1, 1, 1), col= c("black", "red", "blue"))
+    
+    png(file = "Plot3.png")
+    plot(Date2, Sub_Metering_1, type = "n", lty = 1, xlab = "", ylab = "Energy sub metering")
+    lines(Date2, Sub_Metering_1, col = "black")
+    lines(Date2, Sub_Metering_2, col = "red")
+    lines(Date2, Sub_Metering_3, col = "blue")
+    legend( "topright", c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3"), lty = c(1, 1, 1), col= c("black", "red", "blue"))
+    dev.off()
+}
